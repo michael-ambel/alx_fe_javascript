fb9195f8015d4c7c7c755d1be950b8f31f9b71cb
michael-ambel <163504211+michael-ambel@users.noreply.github.com>
2024-07-26 10:01:34 +0300

    task 0

---
+++ dom-manipulation/script.js
@@ -0,0 +1,13 @@
+
+
+const addQuote = () => {
+    
+}
+
+const showRandomQuote = () => {
+    const quoteDisplay = document.getElementById('quoteDisplay');
+}
+
+const showNewBtn = document.getElementById('newQuote');
+
+showNewBtn.addEventListener('click', quoteDisplay);
