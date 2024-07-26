efc1bb6416c8431e584f62bec154486d42313a61
michael-ambel <163504211+michael-ambel@users.noreply.github.com>
2024-07-26 10:08:44 +0300

    0task0

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,8 +1,10 @@
 
 
 const addQuote = () => {
-    
+
 }
+
+const quots = [{text: 'The only way to do great work is to love what you do.', catagory:'Inspiration'}]
 
 const showRandomQuote = () => {
     const quoteDisplay = document.getElementById('quoteDisplay');
