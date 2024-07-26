eea36f4ec8976d65cb7d2a1849adab1c8922ea54
michael-ambel <163504211+michael-ambel@users.noreply.github.com>
2024-07-26 10:47:21 +0300

    task 0

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -4,12 +4,24 @@ const addQuote = () => {
 
 }
 
-const quots = [{"text": 'The only way to do great work is to love what you do.', "category":'Inspiration'}]
+const quots = [
+    {"text": 'The only way to do great work is to love what you do.', 
+    "category":'Work Ethic'}, 
+    {"text": 'In the middle of difficulty lies opportunity.', 
+    "category":'Inspiration'}, 
+    {"text": 'The only person you are destined to become is the person you decide to be.', 
+    "category":'Motivational'}
+]
 
 const showRandomQuote = () => {
     const quoteDisplay = document.getElementById('quoteDisplay');
+    const randomQuoteIndex = Math.floor(Math.random() * quots.length);
+    console.log(randomQuoteIndex);
+    const random = quots[randomQuoteIndex].text;
+    console.log(random);
+    quoteDisplay.innerHTML = random;
 }
 
 const showNewBtn = document.getElementById('newQuote');
 
-showNewBtn.addEventListener('click', quoteDisplay);
+showNewBtn.addEventListener('click', showRandomQuote);
